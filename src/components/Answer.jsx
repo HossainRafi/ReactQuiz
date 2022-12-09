7e61938f82022524f1d7a9e8be3fc9b7bb8beec7
HossainRafi <hossainrafi71@gmail.com>
2022-12-09 19:57:25 +0600

    variable

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -5,10 +5,14 @@ const Answer = ({
   currentAnswer,
   correctAnswer,
 }) => {
-  
-  return (
-    <div></div>
-  );
+  const letterMapping = ["A", "B", "C", "D"];
+  const isCorrectAnswer = currentAnswer && answerText === correctAnswer;
+  const isWrongAnswer =
+    currentAnswer === answerText && currentAnswer !== correctAnswer;
+  const correctAnswerClass = isCorrectAnswer ? "correct-answer" : "";
+  const wrongAnswerClass = isWrongAnswer ? "wrong-answer" : "";
+  const disabledClass = currentAnswer ? "disabled-answer" : "";
+  return <div></div>;
 };
 
 export default Answer;
