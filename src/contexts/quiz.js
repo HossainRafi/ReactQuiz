94437ef1f5350923e4839bc3df2621696a89e85b
HossainRafi <hossainrafi71@gmail.com>
2022-12-09 19:19:18 +0600

    initial state

--- src/contexts/quiz.js
+++ src/contexts/quiz.js
@@ -1,6 +1,10 @@
 import React, { createContext } from "react";
+import questions from "../data";
 
-
+const initialState = {
+  questions,
+  currentQuestionIndex: 0,
+};
 
 export const QuizContext = createContext();
 export const QuizProvider = ({ children }) => {
