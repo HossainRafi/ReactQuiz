fba5aa011c14c563401db42ea335dff155457da5
HossainRafi <hossainrafi71@gmail.com>
2022-12-09 20:14:15 +0600

    select answer case

--- src/contexts/quiz.js
+++ src/contexts/quiz.js
@@ -1,14 +1,39 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 import questions from "../data";
+import { shuffleAnswers } from "../helpers";
 
 const initialState = {
   questions,
   currentQuestionIndex: 0,
+  currentAnswer: "",
+  answers: shuffleAnswers(questions[0]),
+  showResults: false,
+  correctAnswersCount: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SELECT_ANSWER": {
+      const correctAnswersCount =
+        action.payload ===
+        state.questions[state.currentQuestionIndex].correctAnswer
+          ? state.correctAnswersCount + 1
+          : state.correctAnswersCount;
+      return {
+        ...state,
+        currentAnswer: action.payload,
+        correctAnswersCount,
+      };
+    }
+    default:
+      return state;
+  }
 };
 
 export const QuizContext = createContext();
-export const QuizProvider = ({ children }) => {
-  
 
-  return <QuizContext.Provider >{children}</QuizContext.Provider>;
+export const QuizProvider = ({ children }) => {
+  const value = useReducer(reducer, initialState);
+
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };
