c1162c7971bbc6a171a548781e276831c9c8f874
HossainRafi <hossainrafi71@gmail.com>
2022-12-09 20:00:55 +0600

    answer done

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -12,7 +12,15 @@ const Answer = ({
   const correctAnswerClass = isCorrectAnswer ? "correct-answer" : "";
   const wrongAnswerClass = isWrongAnswer ? "wrong-answer" : "";
   const disabledClass = currentAnswer ? "disabled-answer" : "";
-  return <div></div>;
+  return (
+    <div
+      className={`answer ${correctAnswerClass} ${wrongAnswerClass} ${disabledClass}`}
+      onClick={() => onSelectAnswer(answerText)}
+    >
+      <div className="answer-letter">{letterMapping[index]}</div>
+      <div className="answer-text">{answerText}</div>
+    </div>
+  );
 };
 
 export default Answer;
